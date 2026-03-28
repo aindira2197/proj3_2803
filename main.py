26ca45c0ad5141afbe7e01d2817ebb1b06b16f29
Azadova Indira <aindira2197@gmail.com>
2026-03-28 14:59:56 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+talaba = {
+    "ism": "Ali",
+    "yosh": 20
+}
+
+talaba["yosh"] = 21
+print(talaba)
+
+
+talaba = {
+    "ism": "Ali",
+    "yosh": 20,
+    "kurs": 2
+}
+
+del talaba["kurs"]
+print(talaba)
